2a8808226464455eaba2810f2b5f6ce14381d496
connortremblay <90720348+connortremblay@users.noreply.github.com>
2023-06-30 19:38:10 -0400

    Report

--- bash/systeminfo.sh
+++ bash/systeminfo.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-#Final Script Assignment Connor Tremblay 200451942
 
-#This function showslabeled data showing CPU manufacturer and model, CPU architecture, CPU core count
-#CPU maximum speed in a human friendly format
-#Sizes of caches (L1, L2, L3) in a human friendly format
+#Connor Tremblay 200451942
 
-#Function #1
-#This function states some information about your computers CPU.
-function cpureport {
-#Searches for CPU informartion and uses grep to show line including model name
-#Gets rid of the space before the line
-lscpu | grep 'Model name' | awk '{$1=$1}1'
-echo "CPU Cores: $(grep -c ^processor /proc/cpuinfo)"
+#Source original file
+source ~/COMP2101/bash/reportfunctions.sh
 
-#Searches for specified key wordsand remove the space before line
-cachesize=$(lscpu | grep 'L1d\|L2\|L3\|L1i' | awk '{$1=$1}1')
-#list all cache sizes in a friendly manor
-echo -e "Cache Sizes:\n$cachesize"
-}
+#Tests for root using if/else statument and "EUID" and exits if flase.
+if [[ "$EUID" = 0 ]]; then
+    echo "Proceed..."
+else
+    sudo -k 
+    if sudo true; then
+        echo "Correct Password"
+    else
+        echo "Wrong Password, This script must be run as root. GoodBye :)"
+        exit 1
+    fi
+fi
 
+#List of functions
 cpureport
-#End
-
-
+computerreport
+osreport
+ramreport
+videoreport
+diskreport
+networkreport
